e7b95121274c54752c4e75223ec8c61a62955528
Brian Henderson <henderson.brian82@gmail.com>
2020-08-24 15:52:00 -0400

    Added Second Operator Exercise

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = 20;
+            var pi = Math.PI;
+
+            var areaOfCircle = CalculateArea(r);
+
+            Console.WriteLine($"The area of a circle with radius {r} is {areaOfCircle}");
+        }
+
+    public static double CalculateArea(double radius) 
+        {
+            return Math.PI * (radius * radius);
+        }
+    }
+}
